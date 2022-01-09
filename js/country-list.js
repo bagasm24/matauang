e8fe4f0bafc697a69eee5dd28ca5b54d793a7be2
bagasm24 <mbagas368@gmail.com>
2022-01-09 16:25:41 +0700

    first commit

---
+++ js/country-list.js
@@ -0,0 +1,13 @@
+let country_list = {
+    "IDR" : "id",
+    "USD" : "us",
+    "EUR" : "uk",
+    "JPY" : "ja",
+    "KRW" : "ks",
+    "MYR" : "my",
+    "PHP" : "rp",
+    "SGD" : "sn",
+    "THB" : "th",
+    "VND" : "vm",
+    "BND" : "bx"
+}
